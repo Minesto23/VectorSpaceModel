454afbbeef4f868ce2b3e82f0ce2b465c5434b17
Miguel Morales <minesto23@gmail.com>
2022-06-22 21:24:24 +0200

    new version

--- src/core/Test.java
+++ src/core/Test.java
@@ -1,6 +1,5 @@
 package core;
 
-import java.awt.event.ItemEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -13,7 +12,6 @@ import java.util.Set;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		// Reading File information
 		String[] data1 = readFile("WordCount1.txt").split(" ");
@@ -25,64 +23,30 @@ public class Test {
 
 		ArrayList <String> data = new ArrayList<String>();
 		
-		for(int i = 0 ; i < data1.length; i++)
-			if(data1[i] != "")
-				data.add(data1[i].toLowerCase());
-		
-		for(int i = 0 ; i < data2.length; i++)
-			if(data2[i] != "")
-				data.add(data2[i].toLowerCase());
-		
-		for(int i = 0 ; i < data3.length; i++)
-			if(data3[i] != "")
-				data.add(data3[i].toLowerCase());
+		addData(data, data1);
+		addData(data, data2);
+		addData(data, data3);
 		
 		// cleaning duplicate data
 		
 		Set <String> dataset = new HashSet<String>(data);
 		
-		System.out.println(dataset);
 		
 		// Step 1: compute term frequency matrix:
 		
 		Map<String, Integer> map1 = new HashMap<String, Integer>();
 		Map<String, Integer> map2 = new HashMap<String, Integer>();
 		Map<String, Integer> map3 = new HashMap<String, Integer>();
+		
+		
 		int frecuency = 0;
 		
-		for(String item : dataset) {
-			frecuency = 0;
-			for(int i = 1; i < data1.length; i++) {
-				if (item.equals(data1[i])) {
-					frecuency ++;
-				}
-			}
-			map1.put(item, frecuency);
-		}
+		frecuencyMatrix(data1, dataset, map1);
+		frecuencyMatrix(data2, dataset, map2);
+		frecuencyMatrix(data3, dataset, map3);
 		
-		for(String item : dataset) {
-			frecuency = 0;
-			for(int i = 1; i < data2.length; i++) {
-				if (item.equals(data2[i])) {
-					frecuency ++;
-				}
-			}
-			map2.put(item, frecuency);
-		}
 		
-		for(String item : dataset) {
-			frecuency = 0;
-			for(int i = 1; i < data3.length; i++) {
-				if (item.equals(data3[i])) {
-					frecuency ++;
-				}
-			}
-			map3.put(item, frecuency);
-		}
-		
-		//System.out.println(map1);
-		//System.out.println(map2);
-		//System.out.println(map3);
+		niceprint(dataset, map1, map2, map3);
 		
 		
 		// Step 2: Compute the inverse document frequency IDF 
@@ -98,6 +62,7 @@ public class Test {
 			IDFmap.put(key, value);
 		}
 		
+		
 		double valueLog = 0;
 		
 		Map<String, Double> IDFmaplog = new HashMap<String, Double>();
@@ -105,46 +70,14 @@ public class Test {
 		Map<String, Double> IDFmap2 = new HashMap<String, Double>();
 		Map<String, Double> IDFmap3 = new HashMap<String, Double>();
 		
+		computeLog(IDFmap, IDFmaplog);
+		computeLog(map1, IDFmap1);
+		computeLog(map2, IDFmap2);
+		computeLog(map3, IDFmap3);
 		
-		for(String key : IDFmap.keySet()){
-			
-			if(IDFmap.get(key)!=0)
-				valueLog = Math.log((double) 3/IDFmap.get(key)) / Math.log(2);
-			else
-				valueLog = 0;
-			
-			IDFmaplog.put(key, valueLog);
-		}
+		niceprint(IDFmaplog);
 		
-		for(String key : map1.keySet()){
-			
-			if(map1.get(key)!=0)
-				valueLog = Math.log((double) 3/IDFmap.get(key)) / Math.log(2);
-			else
-				valueLog = 0;
-			
-			IDFmap1.put(key, valueLog);
-		}
-		
-		for(String key : map2.keySet()){
-			
-			if(map3.get(key)!=0)
-				valueLog = Math.log((double) 3/IDFmap.get(key)) / Math.log(2);
-			else
-				valueLog = 0;
-			
-			IDFmap2.put(key, valueLog);
-		}
-		
-		for(String key : map3.keySet()){
-			
-			if(map1.get(key)!=0)
-				valueLog = Math.log((double) 3/IDFmap.get(key)) / Math.log(2);
-			else
-				valueLog = 0;
-			
-			IDFmap3.put(key, valueLog);
-		}
+		niceprint2(dataset, IDFmap1, IDFmap2, IDFmap3);
 		
 		/// Step 3: Compute the length of the document vector 
 		
@@ -166,13 +99,19 @@ public class Test {
 		d2 = Math.sqrt(d2);
 		d3 = Math.sqrt(d3);
 		
-		//System.out.println(d1);
-		//System.out.println(d2);
-		//System.out.println(d3);
+		System.out.println("Length of the document vector: \n\nd1 = " + d1 + "\nd2 = " + d2 + "\nd3 = " + d3 );
 		
-		String query = "Hadoop";
+	
+		// query search 
 		
-		String[] Query = query.split("elephant");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please insert your query:");
+		String query = scan.nextLine();
+		
+		String[] Query = query.split(" ");
+		
+		scan.close();
 		
 		ArrayList <String> queryList = new ArrayList<String>();
 		
@@ -181,9 +120,10 @@ public class Test {
 		
 		Set <String> querySet = new HashSet <String>(queryList);
 		
+		
 		Map <String, Integer> queryMap = new HashMap<String, Integer>();
 		
-		double q =0;
+		double q = 0;
 		
 		int queryFrecuency = 0;
 		for(String itemset : querySet) {
@@ -201,43 +141,239 @@ public class Test {
 				queryFrecuency = queryMap.get(key);
 		
 		for(String key : queryMap.keySet())
-			for(String IDFKey : IDFmaplog.keySet())
-				if(IDFKey.equals(key))
-					q += (double) (queryMap.get(key)/queryFrecuency) * IDFmaplog.get(IDFKey);
+			for(String IDFKey : IDFmaplog.keySet()) 
+				if(IDFKey.equals(key)) 
+					q += Math.pow((double) (queryMap.get(key)/queryFrecuency) * IDFmaplog.get(IDFKey), 2);
+			
+			
+		q = Math.sqrt(q);
+		
+		System.out.println("q = " + q);
 		
 		// cosine funtion
 		
-		double cosine1 = 0;
-		double cosine2 = 0;
-		double cosine3 = 0;
+		double cosine1 = cosine(queryMap, IDFmap1, d1, q);
+		double cosine2 = cosine(queryMap, IDFmap2, d2, q);
+		double cosine3 = cosine(queryMap, IDFmap3, d3, q);
+		
+		System.out.println("\nCosine Function:");
+		System.out.println("Cosine d1 = " + cosine1);
+		System.out.println("Cosine d2 = " + cosine2);
+		System.out.println("Cosine d3 = " + cosine3);
+		
+		
+		
+		result(q, cosine1, cosine2, cosine3);
+		
+		
+		
+			
+		
+		
+
+		
+		
+	}
+	
+	public static String readFile(String filename) {
+		
+		String data="";
+		try {
+		      File file = new File(filename);
+		      Scanner myReader = new Scanner(file);
+		      while (myReader.hasNextLine()) {
+		        data += " " + myReader.nextLine();
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		return data;
+	}
+	
+	public static void addData(ArrayList<String> list, String [] data) {
+		
+		for(int i = 0 ; i < data.length; i++)
+			if(data[i] != "") {
+				if(data[i].endsWith("!"))
+					list.add(data[i].replace("!", "").toLowerCase());
+				else if(data[i].endsWith(","))
+					list.add(data[i].replace(",", "").toLowerCase());
+				else if(data[i].endsWith("."))
+					list.add(data[i].replace(".", "").toLowerCase());
+				else	
+					list.add(data[i].toLowerCase());
+			}
+				
+	}
+	
+	public static void frecuencyMatrix(String [] data, Set <String> dataset, Map <String, Integer> map) {
+		int frecuency = 0;
+		
+		for(String item : dataset) {
+			frecuency = 0;
+			for(int i = 1; i < data.length; i++) {
+				if (item.equals(data[i].toLowerCase()))
+					frecuency ++;
+				else if(item.equals(data[i].replace("!", "").toLowerCase()))
+					frecuency ++;
+				else if(item.equals(data[i].replace(".", "").toLowerCase()))
+					frecuency ++;
+				else if(item.equals(data[i].replace(",", "").toLowerCase()))
+					frecuency ++;
+				
+			}
+			map.put(item, frecuency);
+		}
+	}
+	
+	public static void niceprint(Set <String> dataset, Map <String, Integer>  map1, Map <String, Integer>  map2, Map <String, Integer>  map3) {
+		
+		
+		System.out.println("term frequency matrix:");
+		System.out.println();
+		System.out.print("   |");
+		for(String item : dataset)
+			System.out.print(item + "|");
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		String space = "";
+		System.out.print("D1 |");
+		for(String key : map1.keySet()) {
+			for(int i = 0; i < key.length()-1; i++)
+				space += " ";
+			System.out.print(space + map1.get(key) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		System.out.print("D2 |");
+		for(String key : map2.keySet()) {
+			for(int i = 0; i < key.length()-1; i++)
+				space += " ";
+			System.out.print(space + map2.get(key) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		
+		System.out.println();
+		System.out.print("D3 |");
+		for(String key : map3.keySet()) {
+			for(int i = 0; i < key.length()-1; i++)
+				space += " ";
+			System.out.print(space + map3.get(key) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		System.out.println();
+		
+	}
+	
+	public static void computeLog(Map <String,Integer> IDFmap, Map <String,Double> IDFmaplog) {
+		
+		double valueLog;
+		
+		for(String key : IDFmap.keySet()){
+			
+			if(IDFmap.get(key)!=0)
+				valueLog = Math.log((double) 3/IDFmap.get(key)) / Math.log(2);
+			else
+				valueLog = 0;
+			
+			IDFmaplog.put(key, valueLog);
+		}
+		
+	}
+	
+	public static void niceprint(Map<String, Double> IDFmaplog ) {
+		
+		System.out.println("Compute the IDF");
+		System.out.println();
+		for(String key : IDFmaplog.keySet())
+			System.out.println(key + " = " + IDFmaplog.get(key));
+		
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void niceprint2(Set <String> dataset, Map <String, Double>  map1, Map <String, Double>  map2, Map <String, Double>  map3) {
+		
+		System.out.println("Inverse document frequency:");
+		System.out.println();
+		System.out.print("   |");
+		for(String item : dataset)
+			System.out.print(item + "|");
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		String space = "";
+		System.out.print("D1 |");
+		for(String key : map1.keySet()) {
+			for(int i = 0; i < key.length()-5; i++)
+				space += " ";
+			System.out.print(space + String.format("%.02f", map1.get(key)) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		System.out.print("D2 |");
+		for(String key : map2.keySet()) {
+			for(int i = 0; i < key.length()-5; i++)
+				space += " ";
+			System.out.print(space + String.format("%.02f", map2.get(key)) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		
+		System.out.println();
+		System.out.print("D3 |");
+		for(String key : map3.keySet()) {
+			for(int i = 0; i < key.length()-5; i++)
+				space += " ";
+			System.out.print(space + String.format("%.02f", map3.get(key)) + "|");
+			space ="";
+		}
+		System.out.println();
+		for(int i =0; i<285; i ++)
+			System.out.print("=");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+	public static double cosine(Map <String,Integer> queryMap, Map <String,Double> IDFmap, double d, double q) {
+		
+		double cosine=0;
 		
 		for(String key : queryMap.keySet())
-			for(String IDFKey : IDFmap1.keySet())
+			for(String IDFKey : IDFmap.keySet())
 				if(IDFKey.equals(key))
-					cosine1 += (double) queryMap.get(key) * IDFmap1.get(IDFKey);
-		cosine1 = (double) cosine1 / (d1*q);
+					cosine += (double) queryMap.get(key) * IDFmap.get(IDFKey);
+		cosine = (double) cosine / (d*q);
+		return cosine;
+	}
+	
+	public static void result(double q, double cosine1, double cosine2, double cosine3) {
+		double d1 = q - cosine1;
+		double d2 = q - cosine2;
+		double d3 = q - cosine3;
 		
-		for(String key : queryMap.keySet())
-			for(String IDFKey : IDFmap2.keySet())
-				if(IDFKey.equals(key))
-					cosine2 += (double) queryMap.get(key) * IDFmap2.get(IDFKey);
-		cosine2 = (double) cosine2 / (d2*q);
-		
-		for(String key : queryMap.keySet())
-			for(String IDFKey : IDFmap3.keySet())
-				if(IDFKey.equals(key))
-					cosine3 += (double) queryMap.get(key) * IDFmap3.get(IDFKey);
-		cosine3 = (double) cosine3 / (d3*q);
-		
-		
-		//System.out.println(q);
-		//System.out.println(cosine1);
-		//System.out.println(cosine2);
-		//System.out.println(cosine3);
-		
-		d1 = q - cosine1;
-		d2 = q - cosine2;
-		d3 = q - cosine3;
 		
 		double lower = d1;
 		
@@ -274,35 +410,8 @@ public class Test {
 				result += " WordCount2.txt, WordCount1.txt,";
 		}
 		
+		System.out.println();
+		System.out.println("Your index search is: ");
 		System.out.println(result);
-		
-		
-		
-			
-		
-		
-
-		
-		
 	}
-	
-	public static String readFile(String filename) {
-		
-		String data="";
-		try {
-		      File file = new File(filename);
-		      Scanner myReader = new Scanner(file);
-		      while (myReader.hasNextLine()) {
-		        data += " " + myReader.nextLine();
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		
-		return data;
-	}
-	
-
 }
